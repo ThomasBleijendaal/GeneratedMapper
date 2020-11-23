d1f9ba1262c570480dbc1f4653422af14388293a
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2020-11-23 22:06:26 +0100

    0.1.4-preview

---
+++ src/ExampleNuget/Program.cs
@@ -0,0 +1,28 @@
+﻿using GeneratedMapper.Attributes;
+using System;
+
+namespace ExampleNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var s = new Source();
+
+            s.MapToDestination();
+        }
+    }
+
+    [MapTo(typeof(Destination))]
+    public class Source
+    {
+        public string Name { get; set; }
+    }
+
+    public class Destination
+    {
+        public string Name { get; set; }
+    }
+}
